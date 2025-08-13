a627fe85b6b2b977866adc70f2f3dd122f0b68fc
Vesna Tan <tanvezi@gmail.com>
2025-08-13 16:20:42 -0400

    updated asset prefix

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: "standalone",
+    assetPrefix: process.env.BASE_PATH || '',
     images: {
         remotePatterns: [
             {
